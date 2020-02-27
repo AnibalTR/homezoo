dfe2eb79fc95d319b81cccbef145499296d82059
AnibalTR <aanibaltinoco@gmail.com>
2020-02-27 13:19:35 -0700

    Added File User info for environment

--- neumont/edu/csc150/c/controller/PersonalZooController.java
+++ neumont/edu/csc150/c/controller/PersonalZooController.java
@@ -32,13 +32,13 @@ public class PersonalZooController {
                     login();
                     break;
                 case 2:
-                    signup();
+                    signUp();
                     break;
             }
         } while (!exitRequested);
     }
 
-    private void signup() throws IOException {
+    private void signUp() throws IOException {
         User newUser = new User();
         int minNameLen = 3;
         boolean userDoesNotExist;
@@ -63,7 +63,9 @@ public class PersonalZooController {
     }
 
     private void login() {
-        
+        // user enters there credtials
+        // it searches for users file
+        // if it finds that
 
     }
 
