4c802b5e0267a6a9d5d9bd0aa48d6bef522e6767
Michael Olson <michaelolson.1996@protonmail.com>
2020-02-27 09:59:56 -0700

    implemented interface

--- neumont/edu/csc150/c/models/Pet.java
+++ neumont/edu/csc150/c/models/Pet.java
@@ -1,4 +1,19 @@
 package neumont.edu.csc150.c.models;
 
-public class Pet {
+public class Pet implements PetInterface {
+
+    @Override
+    public void eat(Food food) {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void wakeUp() {
+
+    }
 }
--- neumont/edu/csc150/c/models/PetInterface.java
+++ neumont/edu/csc150/c/models/PetInterface.java
@@ -1,7 +1,6 @@
 package neumont.edu.csc150.c.models;
 
 public interface PetInterface {
-
     void eat(Food food);
 
     void sleep();
