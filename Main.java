b79417a2f6f256838401bf28504fd8ee1a1ebff6
Michael Olson <michaelolson.1996@protonmail.com>
2020-03-04 10:39:27 -0700

    placed start function outside of shutdown thread

--- Main.java
+++ Main.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 public class Main {
     private static PersonalZooController zooGame = new PersonalZooController();
     public static void main(String[] args) throws IOException {
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                try {
-                    zooGame.start();
-                    zooGame.serializeUser();
-                } catch(Exception e) {
-                    System.out.println(e);
-                }
+        zooGame.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                zooGame.serializeUser();
+            } catch(Exception e) {
+                System.out.println(e);
             }
         }, "Shutdown-thread"));
     }
